86b0fce18e08b70eda68f05fd6ec47a42c1c5e41
Santoshi-220430 <gurazalasrisantoshi@gmail.com>
2026-01-05 12:45:04 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+/* functions practice */
+//named functions
+function greet(){
+    return "hello";
+}
+console.log(greet());
+
+//Anonymous functions
+const anonymous=function(){
+    return "hi siri";
+}
+console.log(anonymous());
+
+//Arrow functions
+const arrow=(n)=>{
+    return n*n;
+
+}
+console.log(arrow(5))
+
